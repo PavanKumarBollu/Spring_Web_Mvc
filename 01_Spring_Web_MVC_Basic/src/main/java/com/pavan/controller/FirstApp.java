27f806a8a660aced51c53b6f99913681387c173f
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-02 02:42:12 +0530

    Spring Web Mvc Project one basics

--- 01_Spring_Web_MVC_Basic/src/main/java/com/pavan/controller/FirstApp.java
+++ 01_Spring_Web_MVC_Basic/src/main/java/com/pavan/controller/FirstApp.java
@@ -1,6 +1,8 @@
 package com.pavan.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,4 +17,15 @@ public class FirstApp {
 		modelAndView.setViewName("index");
 		return modelAndView;
 	}
+	
+	@GetMapping(value = "/welcome1")
+	public String welcomeMsg(Model model)
+	{
+		model.addAttribute("msg", "Welcome to Spring Web Mvc This Is @GetMapping ");
+		return "index";
+	}
+	
+	
 }
+
+
