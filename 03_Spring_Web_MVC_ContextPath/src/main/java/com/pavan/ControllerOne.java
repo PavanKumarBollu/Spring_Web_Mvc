18faf05f30cee56cd9b9576c278c33b88d9f93e9
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-02 03:12:53 +0530

    Spring Web Mvc Project Class Level and method Level, Default Request mapping setting up the Context path to give the appication name before the url

---
+++ 03_Spring_Web_MVC_ContextPath/src/main/java/com/pavan/ControllerOne.java
@@ -0,0 +1,28 @@
+package com.pavan;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/first")
+public class ControllerOne {
+		
+	@GetMapping("/controller1")
+	public String wish(Model model)
+	{
+		model.addAttribute("msg", "Specific wish of the Controller One");
+		
+		return "index";
+	}
+	@GetMapping
+	public String genaralWish(Model model)
+	{
+		model.addAttribute("msg", "Genaral wish of the Controller one");
+		return "index";
+	}
+	
+	
+	
+}
---
+++ 03_Spring_Web_MVC_ContextPath/src/main/java/com/pavan/ControllerTwo.java
@@ -0,0 +1,26 @@
+package com.pavan;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/second")
+public class ControllerTwo {
+	
+	@GetMapping("/controller1")
+	public String wish(Model model)
+	{
+		model.addAttribute("msg", "Specifice Wish of the Controller Two");
+		return "index";
+	}
+	@GetMapping
+	public String genaralwish(Model model)
+	{
+		model.addAttribute("msg", "Specifice Wish of the Controller Two");
+		return "index";
+	}
+	
+	
+}
