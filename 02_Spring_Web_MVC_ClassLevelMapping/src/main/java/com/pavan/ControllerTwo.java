cae4446ec90692c45399e5c92d40a78b88cf0b8d
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-02 03:06:41 +0530

    Spring Web Mvc Project Class Level and method Level, Default Request mapping

--- 02_Spring_Web_MVC_ClassLevelMapping/src/main/java/com/pavan/ControllerTwo.java
+++ 02_Spring_Web_MVC_ClassLevelMapping/src/main/java/com/pavan/ControllerTwo.java
@@ -3,14 +3,16 @@ package com.pavan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/second")
 public class ControllerTwo {
 	
-	@GetMapping("/controller2")
+	@GetMapping("/controller1")
 	public String wish(Model model)
 	{
-		model.addAttribute("msg", "Controller Two");
+		model.addAttribute("msg", "Specifice Wish of the Controller Two");
 		return "index";
 	}
 }
