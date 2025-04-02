72eedb7f01b7e681781bb70c5d8be224386d274d
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-02 23:54:19 +0530

    Spring Web Mvc Project On getting the data from the ui to controller using PathParams

--- 06_Spring_Web_MVC_UI_To_Controller_PathParams/src/main/java/com/pavan/PathParams.java
+++ 06_Spring_Web_MVC_UI_To_Controller_PathParams/src/main/java/com/pavan/PathParams.java
@@ -1,8 +1,13 @@
 package com.pavan;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 @Controller
 public class PathParams {
 	
+	public String getBookDetails(Model model)
+	{
+		return "index";
+	}
 }
