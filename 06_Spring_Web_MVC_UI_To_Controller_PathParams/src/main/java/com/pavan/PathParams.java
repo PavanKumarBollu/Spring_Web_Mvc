63da0d2eaa6a840ab52c6b149557cb3cffab05bd
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-02 23:58:49 +0530

    Spring Web Mvc Project On getting the data from the ui to controller using PathParams

--- 06_Spring_Web_MVC_UI_To_Controller_PathParams/src/main/java/com/pavan/PathParams.java
+++ 06_Spring_Web_MVC_UI_To_Controller_PathParams/src/main/java/com/pavan/PathParams.java
@@ -2,13 +2,17 @@ package com.pavan;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PathParams {
 	
-	@GetMapping("/book")
-	public String getBookDetails(Model model)
+	@GetMapping("/book/{bName}/bAuthor")
+	public String getBookDetails(@PathVariable("bName")String bName, @PathVariable("bAuthor") String bAuthor, Model model)
 	{
+		
+		model.addAttribute("query" , bName+" Was Designed By " + bAuthor + " ");
 		return "index";
 	}
 }
