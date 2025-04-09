f8ab38952aed814a68d06827d83d547261b30f83
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-09 15:21:17 +0530

    Springboot Web Mvc Basics

--- 10_Spring_Web_MVC_RequestMapping/src/main/java/com/pavan/service/WishMessageServiceImpl.java
+++ 10_Spring_Web_MVC_RequestMapping/src/main/java/com/pavan/service/WishMessageServiceImpl.java
@@ -1,11 +1,21 @@
 package com.pavan.service;
 
+import java.time.LocalDateTime;
+
 public class WishMessageServiceImpl implements IWishMessageService {
 
 	@Override
 	public String generateWishMsg() {
-		// TODO Auto-generated method stub
-		return null;
+		LocalDateTime ldt = LocalDateTime.now();
+		int hour = ldt.getHour();
+		if (hour < 12)
+			return "Good Morning ";
+		else if (hour < 16)
+			return "Good Afternoon";
+		else if (hour < 20)
+			return "Good Evening";
+		else
+			return "Good Night";
 	}
 
 }
