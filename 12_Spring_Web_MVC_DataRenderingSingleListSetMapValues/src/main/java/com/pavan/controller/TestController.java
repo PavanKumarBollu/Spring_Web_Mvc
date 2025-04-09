53f418927c9ce8d0c23d02fbf938bfed8bbc626d
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-09 20:56:10 +0530

    Springboot Web Mvc Data Rendering form Controller to Ui

--- 12_Spring_Web_MVC_DataRenderingSingleListSetMapValues/src/main/java/com/pavan/controller/TestController.java
+++ 12_Spring_Web_MVC_DataRenderingSingleListSetMapValues/src/main/java/com/pavan/controller/TestController.java
@@ -8,15 +8,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 	
-	@GetMapping
+	@GetMapping(value="/")
 	public String showHome()
 	{
 		return "home";
 	}
 	
-	
+	@GetMapping(value="report")
 	public String showReport(Map<String, Object> model)
 	{
+		model.put("name", "Pavan");
+		model.put("age", 25);
+		model.put("addr", "HYD");
 		return "report";
 	}
 
--- 12_Spring_Web_MVC_DataRenderingSingleListSetMapValues/src/main/webapp/WEB-INF/pages/report.jsp
+++ 12_Spring_Web_MVC_DataRenderingSingleListSetMapValues/src/main/webapp/WEB-INF/pages/report.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8" isELIgnored="false" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +7,8 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+	<h1>${name}</h1>
+	<h1>${age}</h1>
+	<h1>${addr}</h1>
 </body>
 </html>
