dc08916f75a0ad486f6125b98ac2c458edc2617a
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-09 15:43:21 +0530

    Springboot Web Mvc Basics

--- 10_Spring_Web_MVC_RequestMapping/src/main/java/com/pavan/controller/WishController.java
+++ 10_Spring_Web_MVC_RequestMapping/src/main/java/com/pavan/controller/WishController.java
@@ -1,12 +1,15 @@
 package com.pavan.controller;
 
-import java.util.Map;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pavan.service.IWishMessageService;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class WishController {
@@ -37,21 +40,27 @@ public class WishController {
 	 * String wishMsg = service.generateWishMsg(); model.addAttribute("msg",
 	 * wishMsg); return "display"; }
 	 */
+	/*
+	 * @RequestMapping(value="/wish") public String generateMsg(Map<String, Object>
+	 * model) { model.put("msg", service.generateWishMsg()); return "display"; }
+	 */
+	
+	/*
+	 * @RequestMapping(value="/display") public void generateMsg1(Map<String,Object>
+	 * model) { model.put("msg", service.generateWishMsg());
+	 * 
+	 * }
+	 */
+	
 	
 	@RequestMapping(value="/wish")
-	public String generateMsg(Map<String, Object> model)
+	public void generateMsg(HttpServletResponse response) throws IOException
 	{
-		model.put("msg", service.generateWishMsg());
-		return "display";
-	}
-	
-	@RequestMapping(value="/display")
-	public void generateMsg1(Map<String,Object> model)
-	{
-		model.put("msg", service.generateWishMsg());
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
 		
+		writer.println("<b> Wish Message : " + service.generateWishMsg() +"</b> "  );
 	}
-	
 	
 	
 }
