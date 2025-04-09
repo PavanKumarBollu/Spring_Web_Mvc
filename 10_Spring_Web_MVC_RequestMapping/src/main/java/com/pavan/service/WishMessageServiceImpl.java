c2b85348ad1185f22ec51403ef88c482a5e8bebe
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-09 15:20:55 +0530

    Springboot Web Mvc Basics

---
+++ 10_Spring_Web_MVC_RequestMapping/src/main/java/com/pavan/service/WishMessageServiceImpl.java
@@ -0,0 +1,11 @@
+package com.pavan.service;
+
+public class WishMessageServiceImpl implements IWishMessageService {
+
+	@Override
+	public String generateWishMsg() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
