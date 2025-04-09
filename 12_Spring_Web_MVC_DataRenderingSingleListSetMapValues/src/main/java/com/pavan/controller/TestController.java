8f359a228653ba2e612e303a74e530664da178d2
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-09 20:48:19 +0530

    Springboot Web Mvc Data Rendering form Controller to Ui

--- 12_Spring_Web_MVC_DataRenderingSingleListSetMapValues/src/main/java/com/pavan/controller/TestController.java
+++ 12_Spring_Web_MVC_DataRenderingSingleListSetMapValues/src/main/java/com/pavan/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.pavan.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,6 +12,12 @@ public class TestController {
 	public String showHome()
 	{
 		return "home";
+	}
+	
+	
+	public String showReport(Map<String, Object> model)
+	{
+		return "report";
 	}
 
 }
