8390e7c4328ffde8d27b21bc7548d728b0d1db50
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-09 21:09:54 +0530

    Springboot Web Mvc Data Rendering form Controller to Ui

--- 12_Spring_Web_MVC_DataRenderingSingleListSetMapValues/src/main/java/com/pavan/controller/TestController.java
+++ 12_Spring_Web_MVC_DataRenderingSingleListSetMapValues/src/main/java/com/pavan/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.pavan.controller;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,22 @@ public class TestController {
 		model.put("name", "Pavan");
 		model.put("age", 25);
 		model.put("addr", "HYD");
+		
+		String[] names = new String[]{"Pavan","Ravi","Kumar"};
+		
+		model.put("names", names);
+		
+		List<String> subjects = List.of("java","jee","spring", "springboot");
+		model.put("subjects", subjects);
+		
+		Set<Long> mobileNo = Set.of(1234l, 2345l,3456l);
+		model.put("mobileNo", mobileNo);
+		
+		Map<String, Number> ids = Map.of("aadhar", 123454321, "pan", 9879879l , "passport", 56789l);
+		model.put("ids", ids);
+		
+		
+		
 		return "report";
 	}
 
--- 12_Spring_Web_MVC_DataRenderingSingleListSetMapValues/src/main/webapp/WEB-INF/pages/report.jsp
+++ 12_Spring_Web_MVC_DataRenderingSingleListSetMapValues/src/main/webapp/WEB-INF/pages/report.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8" isELIgnored="false" %>
+   <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,8 +8,15 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<h1>${name}</h1>
+	<%-- <h1>${name}</h1>
 	<h1>${age}</h1>
-	<h1>${addr}</h1>
+	<h1>${addr}</h1> --%>
+	
+	<h1 style='color: red; text-align: center;'>Displaying Array values</h1>
+	<c:forEach items="${names }" var="name">
+	
+	</c:forEach>
+	
+	
 </body>
 </html>
