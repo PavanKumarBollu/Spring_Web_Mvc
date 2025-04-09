63a6b4c4e8bd3461da9d5e86a2a7b1f326d4fd10
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-09 15:22:48 +0530

    Springboot Web Mvc Basics

--- 10_Spring_Web_MVC_RequestMapping/src/main/java/com/pavan/controller/WishController.java
+++ 10_Spring_Web_MVC_RequestMapping/src/main/java/com/pavan/controller/WishController.java
@@ -1,11 +1,23 @@
 package com.pavan.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pavan.service.IWishMessageService;
 
 @Controller
 public class WishController {
 
+	@Autowired
+	private IWishMessageService service;
 	
+	
+	@RequestMapping(value="/")
+	public String showHome()
+	{
+		return "home";
+	}
 	
 	
 }
