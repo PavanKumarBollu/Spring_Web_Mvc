0b368464d307a6445c4b525ed72543898046829d
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-09 19:44:43 +0530

    Springboot Web Mvc Basics

--- 11_Spring_Web_MVC_RequestMapping/src/main/java/com/pavan/controller/DemoController.java
+++ 11_Spring_Web_MVC_RequestMapping/src/main/java/com/pavan/controller/DemoController.java
@@ -5,8 +5,10 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value="/demo")
 public class DemoController {
 
 	
--- 11_Spring_Web_MVC_RequestMapping/src/main/java/com/pavan/controller/TestController.java
+++ 11_Spring_Web_MVC_RequestMapping/src/main/java/com/pavan/controller/TestController.java
@@ -1,5 +1,20 @@
 package com.pavan.controller;
 
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/test")
 public class TestController {
 
+	
+	@GetMapping(value="/report")
+	public String showReport(Map<String, Object> model)
+	{
+		model.put("msg", "GetMapping of the report");
+		return "display";
+	}
 }
